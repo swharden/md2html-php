76ec7c9752efcce1a9ec2c8556d97133b05de900
Scott W Harden <swharden@gmail.com>
2020-04-03 23:21:25 -0400

    new server using parsedown

---
+++ demo/website2/demo.md
@@ -0,0 +1,3 @@
+# demo
+
+special text
---
+++ demo/website2/templates/top.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+    </style>
+    <script src='https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js'></script>
+</head>
+
+<body>
