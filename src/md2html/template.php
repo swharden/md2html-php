266ba6a7488ddc4b4b32a02e9c9f3c3e5e4c5789
Scott W Harden <swharden@gmail.com>
2020-04-11 21:56:31 -0400

    use backsplash
    big improvement - background is now a single div

--- src/md2html/template.php
+++ src/md2html/template.php
@@ -13,43 +13,42 @@
 
 <body>
 
-    <div id="pageAboveArticle" style="background-color: #1f3d4b;"> <!-- CUSTOMIZE THIS BACKGROUND COLOR -->
-        <div id="headerBlock">
-            <div id="headerMenu">
-                <div class='menuButton'><a href='<?php echo $siteRoot; ?>/'>Home</a></div>
-                <div class='menuButton'><a href='<?php echo $siteRoot; ?>/demo.md.html'>Demo</a></div>
-                <div class='menuButton'><a href='<?php echo $siteRoot; ?>/folder'>Folder</a></div>
-                <div class='menuButton'><a href='https://github.com/swharden/md2html-php'>GitHub</a></div>
-            </div>
-            <div class='title'><a href='<?php echo $siteRoot; ?>'>md2html-PHP</a></div>
-            <div class='subtitle'>A simple markdown-to-HTML converter using .md.html requests</div>
-            <div id="githubButtonWrapper">
-                <a class="github-button" href="https://github.com/swharden/md2html-php" data-size="large" data-show-count="true" aria-label="Star on GitHub" data-text="Star on GitHub"></a>
+    <!-- CUSTOMIZE THIS BACKGROUND COLOR -->
+    <div id="backsplash" style="background-color: #1f3d4b"></div>
+
+    <div id="content">
+
+        <div id="pageAboveArticle">
+            <div id="headerBlock">
+                <div id="headerMenu">
+                    <div class='menuButton'><a href='<?php echo $siteRoot; ?>/'>Home</a></div>
+                    <div class='menuButton'><a href='<?php echo $siteRoot; ?>/demo.md.html'>Demo</a></div>
+                    <div class='menuButton'><a href='<?php echo $siteRoot; ?>/folder'>Folder</a></div>
+                    <div class='menuButton'><a href='https://github.com/swharden/md2html-php'>GitHub</a></div>
+                </div>
+                <div class='title'><a href='<?php echo $siteRoot; ?>'>md2html-PHP</a></div>
+                <div class='subtitle'>A simple markdown-to-HTML converter using .md.html requests</div>
+                <div id="githubButtonWrapper">
+                    <a class="github-button" href="https://github.com/swharden/md2html-php" data-size="large" data-show-count="true" aria-label="Star on GitHub" data-text="Star on GitHub"></a>
+                </div>
             </div>
         </div>
-        <div id="pageUnderArticle" style="background-color: #1f3d4b;"></div> <!-- CUSTOMIZE THIS BACKGROUND COLOR -->
+
+        <article>
+            <?php echo $md2html->html; ?>
+        </article>
+
+        <footer>
+            <div id="footerBlock">
+                This page was converted from
+                <a href='<?php echo basename($filePath); ?>'><?php echo basename($filePath); ?></a>
+                to HTML by <a href='https://github.com/swharden/md2html-php'>md2html</a>
+                in <?php echo number_format($md2html->benchmarkMsec, 2); ?> milliseconds.
+            </div>
+        </footer>
+        
     </div>
 
-    <article>
-
-        <?php echo $md2html->html; ?>
-
-    </article>
-
-    <footer>
-        <div id="footerBlock">
-            This page
-            <!--<a href='<?php echo basename($reqFile); ?>'><?php echo basename($reqFile); ?></a>-->
-            was converted from
-            <a href='<?php echo basename($filePath); ?>'><?php echo basename($filePath); ?></a>
-            to HTML by
-            <a href='https://github.com/swharden/md2html-php'>md2html</a>
-            <!-- <?php echo $md2html->version; ?> -->
-            in
-            <?php echo number_format($md2html->benchmarkMsec, 2); ?>
-            milliseconds.
-        </div>
-    </footer>
 </body>
 
 </html>
