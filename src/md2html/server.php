cbfdab01b13749ec803bb27452bd416d9da9732b
Scott W Harden <swharden@gmail.com>
2020-06-28 19:19:55 -0400

    improve source code requests
    .md files now aren't served directly. They're wrapped in HTML with a noindex meta tag to prevent search indexing.

--- src/md2html/server.php
+++ src/md2html/server.php
@@ -3,20 +3,35 @@
 error_reporting(-1);
 
 // this file should only get requests ending in .md.html
-$reqFile = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'];
-//echo "<div style='background-color: yellow;'>$reqFile</div>";
+$reqFile = $_SERVER['DOCUMENT_ROOT'] . strtok($_SERVER["REQUEST_URI"], '?');
+$reqPath = strtok($_SERVER["REQUEST_URI"], '?');
+
+// ensure the request ends in .md.html
 if (substr($reqFile, -1) == "/")
     $reqFile .= "readme.md.html";
 if (substr($reqFile, -8) != ".md.html")
     $reqFile .= ".md.html";
 
-// ensure the file exists
+// ensure the underlying markdown file exists
 $filePath = substr($reqFile, 0, -5);
 if (!file_exists($filePath))
     return http_response_code(404);
 
-// convert markdown to HTML and serve the result with the template
+// serve the raw file or the HTML version
 chdir("../");
-require "md2html/md2html.php";
-$md2html = new md2html($filePath);
-include("md2html/template.php");
+if (isset($_GET['source'])) {
+    // if the URL ends in "?source" render the markdown file
+    echo '<html><head><meta name="robots" content="noindex"></head>';
+    echo "<body style='margin: 0px;'>";
+    echo "<div style=\"background-color: #333; color: #EEE; padding: 10px; font-family: sans-serif;\" >";
+    echo "Markdown source for <a href='$reqPath' style='color: #FFF;'>$reqPath</a>";
+    echo "</div><pre style='padding: 0px 20px;'>\n";
+    echo htmlentities(file_get_contents($filePath));
+    echo "\n</pre></body></html>";
+    exit;
+} else {
+    // convert markdown to HTML and serve using the template
+    require "md2html/md2html.php";
+    $md2html = new md2html($filePath);
+    include("md2html/template.php");
+}
