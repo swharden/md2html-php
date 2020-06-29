0b5ddb3fbfaf7e2f1c59365178e9be694d2b1787
Scott W Harden <swharden@gmail.com>
2020-06-28 23:59:40 -0400

    update scripts

---
+++ src/md2html/update.bat
@@ -0,0 +1,9 @@
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/.htaccess > ..\.htaccess
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/Parsedown.php > Parsedown.php
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.css > md2html.css
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.php > md2html.php
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/server.php > server.php
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/sitemap.php > sitemap.php
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.php > template.php
+curl https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.css > template.css
+pause
---
+++ src/md2html/update.sh
@@ -0,0 +1,8 @@
+wget -O ../.htaccess https://raw.githubusercontent.com/swharden/md2html-php/master/src/.htaccess
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/Parsedown.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.css
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/server.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/sitemap.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.css
