11286a987d58e54a3933c16304c1a09c52d3dc9e
Scott W Harden <swharden@gmail.com>
2020-06-29 00:16:52 -0400

    Update update.sh

--- src/md2html/update.sh
+++ src/md2html/update.sh
@@ -1,8 +1,8 @@
-wget -O ../.htaccess https://raw.githubusercontent.com/swharden/md2html-php/master/src/.htaccess
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/Parsedown.php
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.css
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.php
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/server.php
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/sitemap.php
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.php
-wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.css
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/.htaccess -O ../.htaccess
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/Parsedown.php -O Parsedown.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.css -O md2html.css
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/md2html.php -O md2html.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/server.php -O server.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/sitemap.php -O sitemap.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.php -O template.php
+wget https://raw.githubusercontent.com/swharden/md2html-php/master/src/md2html/template.css -O template.css
