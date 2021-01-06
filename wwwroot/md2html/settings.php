25f80ae433e37f03ed6f586c0fe314c1fd6123e5
Scott W Harden <swharden@gmail.com>
2021-01-05 22:11:38 -0500

    default to empty title and description

--- wwwroot/md2html/settings.php
+++ wwwroot/md2html/settings.php
@@ -2,7 +2,7 @@
 
 // these are default template replacements that are overridden by front matter
 return array(
-    '{{title}}' => 'md2html',
+    '{{title}}' => '',
     '{{description}}' => '',
     '{{analyticsID}}' => 'UA-560719-1',
     '{{adsHtml}}' => '<!-- ad code here -->'
