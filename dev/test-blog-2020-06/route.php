92f9077cb384ee11d830b2e1033720df201e5820
Scott W Harden <swharden@gmail.com>
2021-01-17 14:00:23 -0500

    delete old system

--- dev/test-blog-2020-06/route.php
+++
@@ -1,7 +0,0 @@
-<?
-
-// we only get here the URL is a folder with index.md but not index.php
-require_once("SingleBlogPost.php");
-$requested_folder_path = $_SERVER['DOCUMENT_ROOT'] . strtok($_SERVER["REQUEST_URI"], '?');
-$post = new SingleBlogPost($requested_folder_path); 
-echo $post;
