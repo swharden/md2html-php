7bbf6302b9f8d5093eabc03536082d3b0071e656
Scott W Harden <swharden@gmail.com>
2021-02-03 22:22:45 -0500

    improved control of ads, analytics, and indexing

--- md2html/md2html.php
+++ md2html/md2html.php
@@ -4,19 +4,27 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function ServeSingleMarkdownFile(string $mdFilePath, string  $pageTemplate, string $articleTemplate)
-{
-    // create a HTML page around the file
-    require('Page/SingleMarkdownFilePage.php');
+require('Page/SingleMarkdownFilePage.php');
+require('Configuration.php');
+
+function ServeSingleMarkdownFile(
+    string $mdFilePath,
+    string  $pageTemplate,
+    string $articleTemplate,
+    bool $ads = true,
+    bool $analytics = true,
+    bool $robotsIndex = true
+) {
     $page = new SingleMarkdownFilePage($mdFilePath);
     $html = $page->getHtml($pageTemplate, $articleTemplate);
 
-    // tweak the page based on context
-    require('Configuration.php');
     $config = new Configuration();
-    $html = $page->addBeforeClosingHeader($html, $config->TEMPLATE_ADS);
-    $html = $page->addBeforeClosingHeader($html, $config->TEMPLATE_ANALYTICS);
-    //$html = $page->addBeforeClosingHeader($html, $config->TEMPLATE_NOINDEX);
+    if ($ads)
+        $html = $page->addBeforeClosingHeader($html, $config->TEMPLATE_ADS);
+    if ($analytics)
+        $html = $page->addBeforeClosingHeader($html, $config->TEMPLATE_ANALYTICS);
+    if ($robotsIndex == false)
+        $html = $page->addBeforeClosingHeader($html, $config->TEMPLATE_NOINDEX);
 
     echo $html;
 }
