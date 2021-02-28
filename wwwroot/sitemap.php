60f096504469950f241094e5219108a74274aa8b
Scott W Harden <swharden@gmail.com>
2021-02-28 12:19:33 -0500

    disable sitemap caching

--- wwwroot/sitemap.php
+++ wwwroot/sitemap.php
@@ -1,5 +1,9 @@
 <?php
 
+// disable dynamic PHP caching so the markdown file is forced to parse every time
+//header("Cache-Control: max-age=6000");
+header("Cache-Control: no-cache");
+
 require_once('../md2html/Sitemap.php');
 $baseUrl = dirname("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 $sitemap = new Sitemap($baseUrl);
